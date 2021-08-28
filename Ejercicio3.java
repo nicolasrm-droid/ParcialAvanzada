29b995cdd41737372065489198008b86440155b4
nicolasrm-droid <72952718+nicolasrm-droid@users.noreply.github.com>
2021-08-27 22:15:47 -0500

    Add files via upload

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,9 +1,32 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 Scanner teclado = new Scanner(System.in);
+	        int nump = Integer.parseInt(teclado.nextLine());
+	        ArrayList<Integer> dia = new ArrayList<>();
+	        while (nump != 0) {
+	            if (nump >= 2 && nump <= 5) {
+	                String dias = teclado.nextLine();
+	                for (int i = 0; i < nump; i++) {
+	                    dia.add(Integer.parseInt(dias.split(" ")[i]));
+	                }
+	                int i = 2;
+	                int planeta = 0;
+	                while (planeta < (dia.size())) {
+	                    if (i % dia.get(planeta) == 0) {
+	                        planeta++;
+	                    } else {
+	                        i++;
+	                        planeta = 0;
+	                    }
+	                }
+	                System.out.println(i);
+	                dia.clear();
+	            }
+	            nump = Integer.parseInt(teclado.nextLine());
+	        }
+	    }
 	}
-
-}
