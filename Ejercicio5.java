661d76c6e944de2d85955f0c0000c6200bbe6bb2
nicolasrm-droid <72952718+nicolasrm-droid@users.noreply.github.com>
2021-08-27 19:44:51 -0500

    Add files via upload

---
+++ Ejercicio5.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner teclado = new Scanner(System.in);
+		int numero, digito =0;
+		System.out.println("Ingrese un numero: ");
+		numero= teclado.nextInt();
+		
+		
+		if(numero <0) {
+			System.out.println("No escriba numeros negativos :)");
+			return;
+		}
+		else
+			if(numero == 0 ) {
+				System.out.println("no escriba 0 por favor");
+				return ;
+			}
+
+		int aux=numero;
+		
+		while (numero != 0) {
+			digito= digito + (numero % 10 );
+			numero= numero / 10 ;
+			
+		}
+		System.out.println("sumando digitos: " + aux);
+		
+		System.out.println("la suma de los digitos es: " + digito );
+
+
+	}
+
+
+}
